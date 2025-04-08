1e04084caf4ea36ba96fd423884e57f077ad2c83
edenriquez <lalo@palenca.com>
2025-04-07 21:47:14 -0600

    fix: deploy 🗿

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
+    images: { domains: [ "amazonaws.com", ], },
+
 };
 
 export default nextConfig;
--- front/src/components/header.tsx
+++ front/src/components/header.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 export default function Header({ className }: { className?: string }) {
   return (
     <div className={className}>
-      <Image className="mx-auto"
-        src="https://tomin-ai.s3.us-east-1.amazonaws.com/logo.png" 
+      <img className="mx-auto"
+        src="/logo.png"
         alt="Tomin logo"
         width={300}
         height={300}
-        priority
       />
       <h1 className="text-4xl font-bold">tomin</h1>
       <p className="text-lg text-gray-500">AI-powered financial assistant</p>
