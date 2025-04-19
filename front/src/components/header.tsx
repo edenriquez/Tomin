09aed4cab116ea7d09c0e15a4887a09631fe38a7
edenriquez <lalo@palenca.com>
2025-04-18 22:29:57 -0600

    feat: clean up

--- front/src/components/header.tsx
+++ front/src/components/header.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 export default function Header({ className }: { className?: string }) {
   return (
-    <div className={`font-sans ${className}`}>
+    <div className={`relative font-sans ${className}`}>
       <img className="mx-auto"
         src="/logo.png"
         alt="Tomin logo"
         width={300}
         height={300}
       />
-      <p className="text-4xl  text-white">AI-powered financial assistant</p>
+      <p className="text-4xl text-white mt-4">AI-Powered Financial Analysis</p>
     </div>
   );
 }
