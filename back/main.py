771a4f7ccee349191e7960523e82e95001f19820
edenriquez <lalo@palenca.com>
2025-04-21 20:51:47 -0600

    fix: remove redis storage

--- back/main.py
+++ back/main.py
@@ -9,18 +9,13 @@ from database import connection
 from fastapi import HTTPException
 import requests
 import datetime
-import os
 from fastapi import Request
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
 
-limiter = Limiter(
-    key_func=get_remote_address,
-    default_limits=["2/minute"],
-    storage_uri=os.environ.get("REDIS_URL"),
-)
+limiter = Limiter(key_func=get_remote_address, default_limits=["2/minute"])
 
 app = FastAPI()
 app.state.limiter = limiter
